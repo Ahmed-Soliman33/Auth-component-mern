434a1e5fad62b4886fde646f3f78e5212610c471
Ahmed Soliman <ahmedmohamedmahmoud2012005@gmail.com>
2025-06-30 17:41:07 +0300

    u

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,10 +1,11 @@
 import images from "../content/images.json";
+import logo from "../assets/logo.jpg";
 
 export default function Header() {
   return (
     <header>
       <img
-        src={images.logo.src}
+        src={images.logo.src || logo}
         className="mx-auto"
         alt="A form and a pencil"
       />
