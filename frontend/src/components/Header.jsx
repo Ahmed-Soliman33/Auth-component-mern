83190ff0e3cda846f8dd6c30cad9728a0273560e
Ahmed Soliman <ahmedmohamedmahmoud2012005@gmail.com>
2025-06-30 17:42:18 +0300

    u

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,11 +4,7 @@ import logo from "../assets/logo.jpg";
 export default function Header() {
   return (
     <header>
-      <img
-        src={images.logo.src || logo}
-        className="mx-auto"
-        alt="A form and a pencil"
-      />
+      <img src={logo} className="mx-auto" alt="A form and a pencil" />
       <h1>React Forms</h1>
     </header>
   );
